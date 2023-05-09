55d7e7a5408b2b4f14e88f45b67c9e7994ef4745
Maxington20 <max.herrington@gmail.com>
2023-05-08 20:47:21 -0400

    changes to the styling

--- src/components/PromptChatbot.vue
+++ src/components/PromptChatbot.vue
@@ -1,27 +1,34 @@
 <template>
-    <div class="prompt-chatbot">
-      <div class="input-container">
-        <input
-          class="input-field"
-          v-model="prompt"
-          placeholder="Enter your prompt"
-        />
-        <button class="send-button" @click="promptChatbot" :disabled="loading">
-          {{ loading ? "Loading..." : "Send" }}
-        </button>
-      </div>
-      <div class="response-container">
-        <div v-if="isPointForm" class="response">
-          <ul>
-            <li v-for="(point, index) in responsePoints" :key="index">{{ point }}</li>
-          </ul>
+  <div class="app-container">
+    <header class="app-header">
+      <h2>Chat</h2>
+    </header>
+    <div class="content-container">
+      <div class="prompt-chatbot">
+        <div class="response-container">
+          <div v-if="isPointForm" class="response">
+            <ul>
+              <li v-for="(point, index) in responsePoints" :key="index">{{ point }}</li>
+            </ul>
+          </div>
+          <div v-else class="response">
+            <p>{{ response }}</p>
+          </div>
         </div>
-        <div v-else class="response">
-          <p>{{ response }}</p>
+        <div class="input-container">
+          <input
+            class="input-field"
+            v-model="prompt"
+            placeholder="Enter your prompt"
+          />
+          <button class="send-button" @click="promptChatbot" :disabled="loading">
+            {{ loading ? "Loading..." : "Send" }}
+          </button>
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
 
 
 
@@ -93,34 +100,90 @@
 
 
 <style scoped>
+.app-container {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+
+h2 {
+  margin: 0;
+}
+
+.app-header {
+  padding: 0;
+  background-color: #007bff;
+  color: white;
+  font-size: 1.5rem;
+  text-align: center;
+  border-radius: 15px;
+  border-color: #007bff;
+  border: 1px solid #007bff;
+}
+
+.tabs-container {
+  display: flex;
+  justify-content: center;
+  background-color: #f8f9fa;
+  border-bottom: 1px solid #dee2e6;
+  padding: 0.5rem;
+}
+
+.tabs-container button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-size: 1.1rem;
+  color: #333;
+  padding: 0.5rem 1rem;
+  outline: none;
+}
+
+.tabs-container button:hover {
+  color: #007bff;
+}
+
+.tabs-container button.active {
+  color: #007bff;
+  border-bottom: 2px solid #007bff;
+}
+
+.content-container {
+  flex-grow: 1;
+  overflow-y: auto;
+  padding: 1rem 0 0;
+}
 
 .prompt-chatbot {
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
 }
 
 .input-container {
   display: flex;
   justify-content: center;
-  margin-bottom: 1rem;
+  margin-top: 1rem;
   width: 100%;
 }
 
 .input-field {
-  padding: 0.5rem;
-  font-size: 1rem;
+  padding: 0.75rem;
+  font-size: 1.5rem;
   flex-grow: 1;
   border: 1px solid #ccc;
-  border-radius: 3px;
+  border-radius: 15px;
   outline: none;
 }
 
 .send-button {
-  padding: 0.5rem 1rem;
+  padding: 0.5rem 2rem;
   font-size: 1rem;
   color: #fff;
   background-color: #007bff;
-  border: none;
-  border-radius: 3px;
+  border: 1px;
+  border-radius: 15px;
   cursor: pointer;
   outline: none;
   margin-left: 0.5rem;
@@ -132,11 +195,27 @@
 
 .response-container {
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
 }
 
 .response {
   text-align: left;
-  font-size: 1rem;
+  font-size: 1.5rem;
+  background-color: #f0f0f0;
+  border-radius: 15px;
+  padding: 12px 20px;
+  margin-bottom: 8px;
+  word-wrap: break-word;
 }
 
+.response p {
+  margin: 0;
+}
+
+.response ul {
+  margin: 0;
+  padding-left: 20px;
+}
 </style>
